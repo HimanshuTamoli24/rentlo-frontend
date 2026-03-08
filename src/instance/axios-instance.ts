148b3b897b512eaeeb1d34795499f6f92523088c
Himanshu Tamoli <himanshutamoli2005@gmail.com>
2026-03-08 18:30:51 +0530

    bigboss

--- src/instance/axios-instance.ts
+++ src/instance/axios-instance.ts
@@ -9,6 +9,19 @@ export default function createAxiosInstance() {
     withCredentials: true,
   });
 
- 
+  // Adding request interceptor for Authorization header
+  instance.interceptors.request.use(
+    (config) => {
+      const token = localStorage.getItem("token");
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    },
+  );
+
   return instance;
 }
